67372d68da808b58f7d471a0a62c9f940946ac22
ZmbOrk <aleksei05030@gmail.com>
2021-04-08 23:17:43 +1000

    New changes on test for homework

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,25 +1,76 @@
 package ru.netology.stats;
-import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import ru.netology.stats.StatsService;
-import java.util.Arrays;
-
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class StatsServiceTest {
 
-    @ParameterizedTest
+    @Test
     @CsvFileSource
-    void shouldfullSum (String line) {
-        StatsService  service= new StatsService();
-        //конвертируем строку в int
-        int[] monthsSales = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
-        int actual = service.getFullSum(monthsSales);
+    void findFullSumm() {
+        StatsService service = new StatsService();
 
-        int expected = 180;
+        int[] monthsSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual = service.getFullSum(monthsSales);
 
         assertEquals(expected, actual);
     }
+    @Test
+    @CsvFileSource
+    void findAverage() {
+        StatsService service = new StatsService();
 
+        int[] monthsSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.getAverageSales(monthsSales);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    @CsvFileSource
+    void findMonthMaxSales() {
+        StatsService service = new StatsService();
+
+        int[] monthsSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+        long actual = service.getMonthMaxSales(monthsSales);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    @CsvFileSource
+    void findMonthMinSales() {
+        StatsService service = new StatsService();
+
+        int[] monthsSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+        long actual = service.getMonthMinSales(monthsSales);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    @CsvFileSource
+    void findCountOverAverageSales() {
+        StatsService service = new StatsService();
+
+        int[] monthsSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.getCountOverAverageSales(monthsSales);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    @CsvFileSource
+    void findCountUnderAverageSales() {
+        StatsService service = new StatsService();
+
+        int[] monthsSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.getCountUnderAverageSales(monthsSales);
+
+        assertEquals(expected, actual);
+    }
 }
